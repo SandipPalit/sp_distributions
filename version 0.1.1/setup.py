827b29baa8990c3b4c1b1ee2ae61ed1d805f153a
SANDIP PALIT <sandippalit1997@gmail.com>
2020-06-10 19:17:36 +0530

    version 0.1.1 initialise

---
+++ version 0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sp_distributions',
+      version='0.1.1',
+      license='MIT',
+      author='Sandip Palit',
+      author_email='sandippalit009@gmail.com',
+      description='A statistical package to compute Mean and Standard deviation on of various Probability distributions',
+      packages=['sp_distributions'],
+      python_requires='>=3.6',
+      zip_safe=False)
