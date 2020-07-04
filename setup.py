6c32a5ab5dc3c5bcfea9b89834fb1216f7885e83
SANDIP PALIT <sandippalit1997@gmail.com>
2020-07-04 18:02:20 +0530

    version 0.1.2

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open("sp_distributions/README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='sp_distributions',
-      version='0.1.1',
+      version='0.1.2',
       license='MIT',
       author='Sandip Palit',
       author_email='sandippalit009@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       description='A statistical package to compute Mean and Standard deviation on of various Probability distributions',
       packages=['sp_distributions'],
       python_requires='>=3.6',
